2110b202f79c5c46085b1bafc23d4943f39a36e9
Afra Anjum <afraanjum896@gmail.com>
2026-09-26 12:15:55 +0600

    env added

--- src/app/lib/feching.tsx
+++ src/app/lib/feching.tsx
@@ -1,10 +1,17 @@
-export const getAllWorkouts = async()=>{
-    const res = await fetch('http://localhost:3000//data.json');
-    const data = res.json();
+export const getAllWorkouts = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/data.json`
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch workout data");
+    }
+
+    const data = await res.json();
     return data;
-}
-// export const getAllDetails = async()=>{
-//     const res = await fetch('http://localhost:3000//data.json');
-//     const data = res.json();
-//     return data;
-// }
+  } catch (error) {
+    console.error("Error fetching workouts:", error);
+    return [];
+  }
+};
